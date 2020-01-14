166be18e104632edcb75d609e9b5b82fc8010970
yuwei1 <yuweijiubayiqi@163.com>
2020-01-14 20:48:54 +0800

    基础配置文件

---
+++ basic/basic.go
@@ -0,0 +1,11 @@
+package basic
+
+import (
+	"github.com/yuwe1/recycle-shop/basic/client/rediscli"
+	"github.com/yuwe1/recycle-shop/basic/config"
+)
+
+func Init() {
+	config.Init()
+	rediscli.Init()
+}
