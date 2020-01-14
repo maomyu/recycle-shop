91d31d594aa8328102ceaba6d422bcd3fdd57013
yuwei1 <yuweijiubayiqi@163.com>
2020-01-14 22:01:28 +0800

    test

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/yuwe1/recycle-shop
 go 1.13
 
 require (
+	github.com/garyburd/redigo v1.6.0
 	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/micro/go-micro v1.18.0
+	go.uber.org/zap v1.12.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
--- test-ser/main.go
+++ test-ser/main.go
@@ -5,10 +5,15 @@ import (
 
 	"github.com/yuwe1/recycle-shop/basic"
 	"github.com/yuwe1/recycle-shop/basic/client/rediscli"
+	"github.com/yuwe1/recycle-shop/basic/client/rediscli/redispool"
 )
 
 func main() {
 	basic.Init()
 	conn := rediscli.GetRedis()
 	fmt.Println(conn)
+	f, _, p, c := redispool.NewSession()
+	f, _, p, c = redispool.NewSession()
+	defer f.Relase(p, c)
+	fmt.Println(f.GetID())
 }
