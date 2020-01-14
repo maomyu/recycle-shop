e1e9f3e46bede9006ac5df4a045bb6253c8e86c9
yuwei1 <yuweijiubayiqi@163.com>
2020-01-14 22:00:56 +0800

    add redsipool.Init

--- basic/basic.go
+++ basic/basic.go
@@ -2,10 +2,12 @@ package basic
 
 import (
 	"github.com/yuwe1/recycle-shop/basic/client/rediscli"
+	"github.com/yuwe1/recycle-shop/basic/client/rediscli/redispool"
 	"github.com/yuwe1/recycle-shop/basic/config"
 )
 
 func Init() {
 	config.Init()
 	rediscli.Init()
+	redispool.Init()
 }
